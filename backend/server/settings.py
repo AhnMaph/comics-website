3e8a0d361f95db5857a13e183748345d527feed7
Ahn <23520027@gm.uit.edu.vn>
2025-06-23 13:04:42 +0700

    quên dấu phẩy

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -237,9 +237,9 @@ CORS_ALLOW_HEADERS = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "comics-website-production.up.railway.app"
+    "comics-website-production.up.railway.app",
     "https://mangadakh.up.railway.app",
-    "http://empowering-truth.railway.internal:5174"
+    "http://empowering-truth.railway.internal:5174",
     "http://localhost:5174",
 ]
 AUTH_USER_MODEL = 'users.CustomUser'
